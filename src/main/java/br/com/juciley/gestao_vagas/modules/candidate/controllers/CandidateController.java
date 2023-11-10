b19f6aac21bcb1d4b9bb2164b886e272f359e153
juciley.costa <juciley.costa@e-unicred.com.br>
2023-11-10 16:15:46 -0300

    Entity e controller - Candidate

--- src/main/java/br/com/juciley/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/juciley/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,11 +1,21 @@
 package br.com.juciley.gestao_vagas.modules.candidate.controllers;
 
+import br.com.juciley.gestao_vagas.modules.candidate.CandidateEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
+
+	@PostMapping("/")
+	public void create(@RequestBody CandidateEntity candidateEntity) {
+
+		System.out.println("Candidato");
+		System.out.println(candidateEntity.getEmail());
+	}
 
 
 }
