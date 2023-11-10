0b02229649801848ca8b9e142e6ca887ec68aeb9
juciley.costa <juciley.costa@e-unicred.com.br>
2023-11-10 16:41:57 -0300

    Validação de campos - Candidate

--- src/main/java/br/com/juciley/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/juciley/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,6 +1,10 @@
 package br.com.juciley.gestao_vagas.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -9,8 +13,17 @@ public class CandidateEntity {
 
 	private UUID id;
 	private String name;
+
+	// "^(?!\\s*$).+"
+	// "^[^\\s]+$"
+	@NotBlank
+	@Pattern(regexp = "\\S+" , message = "O campo [username] não deve conter espaço.")
 	private String username;
+
+	@Email(message = "O campo [email] deve ser válido.")
 	private String email;
+
+	@Length(min = 10, max = 100, message = "O campo [password] deve conter entre (10) e (100) caracteres.")
 	private String password;
 	private String description;
 	private String curriculum;
