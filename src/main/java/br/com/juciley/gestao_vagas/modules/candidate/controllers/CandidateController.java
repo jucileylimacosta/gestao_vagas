e74cda966df08a515abde1b24db182ec42f2d2cf
juciley.costa <juciley.costa@e-unicred.com.br>
2023-11-10 10:48:23 -0300

    Initial commit

---
+++ src/main/java/br/com/juciley/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,11 @@
+package br.com.juciley.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+
+}
